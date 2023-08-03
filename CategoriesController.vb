6c2635fbd093da4e0deb58fe93c003383144df73
sohailamohamed33 <105173724+sohailamohamed33@users.noreply.github.com>
2023-08-03 15:00:57 +0300

    Add files via upload

---
+++ CategoriesController.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.Entity
+Imports System.Linq
+Imports System.Net
+Imports System.Web
+Imports System.Web.Mvc
+Imports PCProject1
+
+Namespace Controllers
+    Public Class CategoriesController
+        Inherits System.Web.Mvc.Controller
+
+        Private db As New PCProjectEntities
+
+        ' GET: Categories
+        Function Index() As ActionResult
+            Return View(db.Categories.ToList())
+        End Function
+
+        ' GET: Categories/Details/5
+        Function SearchById(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim category As Category = db.Categories.Find(id)
+            If IsNothing(category) Then
+                Return HttpNotFound()
+            End If
+            Return View(category)
+        End Function
+
+        ' GET: Categories/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Categories/Create
+        'To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        'more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        <HttpPost()>
+        <ValidateAntiForgeryToken()>
+        Function Create(<Bind(Include:="Id,CategoryType")> ByVal category As Category) As ActionResult
+            If ModelState.IsValid Then
+                db.Categories.Add(category)
+                db.SaveChanges()
+                Return RedirectToAction("Index")
+            End If
+            Return View(category)
+        End Function
+
+        ' GET: Categories/Edit/5
+        Function Edit(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim category As Category = db.Categories.Find(id)
+            If IsNothing(category) Then
+                Return HttpNotFound()
+            End If
+            Return View(category)
+        End Function
+
+        ' POST: Categories/Edit/5
+        'To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        'more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        <HttpPost()>
+        <ValidateAntiForgeryToken()>
+        Function Edit(<Bind(Include:="Id,CategoryType")> ByVal category As Category) As ActionResult
+            If ModelState.IsValid Then
+                db.Entry(category).State = EntityState.Modified
+                db.SaveChanges()
+                Return RedirectToAction("Index")
+            End If
+            Return View(category)
+        End Function
+
+        ' GET: Categories/Delete/5
+        Function Delete(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim category As Category = db.Categories.Find(id)
+            If IsNothing(category) Then
+                Return HttpNotFound()
+            End If
+            Return View(category)
+        End Function
+
+        ' POST: Categories/Delete/5
+        <HttpPost()>
+        <ActionName("Delete")>
+        <ValidateAntiForgeryToken()>
+        Function DeleteConfirmed(ByVal id As Integer) As ActionResult
+            Dim category As Category = db.Categories.Find(id)
+            db.Categories.Remove(category)
+            db.SaveChanges()
+            Return RedirectToAction("Index")
+        End Function
+
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If (disposing) Then
+                db.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+    End Class
+End Namespace
